ef2b593c52feeb5fe4d83e746b1dfa29d45da41c
Kris Winbush <khz@kriss-mbp.lan>
2024-01-10 15:38:24 -0800

    added JWT  authentication with cookies and user signin form

--- src/main.ts
+++ src/main.ts
@@ -3,18 +3,19 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
 import path = require('path');
 import { join } from 'path';
-
+import * as cookieParser from 'cookie-parser';
 import * as hbs from 'hbs';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.use(cookieParser());
   app.setBaseViewsDir(path.join(__dirname, '../', 'views'));
   hbs.registerPartials(path.join(__dirname, '../', 'views/partials'));
   app.setViewEngine('hbs');
 
   app.useStaticAssets(join(__dirname, '..', '/public'));
 
-  app.set('view options', { layout: 'main' });
+  app.set('view options', { layout: 'index' });
 
   await app.listen(3000);
 }
